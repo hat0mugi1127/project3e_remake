872be5df854b22002778c9b8934050f6d3d5554c
hatomugi1127 <hatomugi.work@gmail.com>
2020-08-05 00:04:26 +0900

    project3E_Remake
    project3Eのリメイク版
    (シンプルなソースを書く、UnityからGitHubへコミットの練習も兼ねて)

---
+++ Assets/Script/Motion_Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion_Enemy : MonoBehaviour {
+    //移動速度
+    public float speed_e;
+
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-speed_e * Time.deltaTime, 0, 0);
+        Vector3 pos = transform.position;      
+        if(pos.x <= -10) {
+            transform.position = new Vector3(10, pos.y, 0);
+        }
+	}
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Vector3 pos = transform.position;
+        transform.position = new Vector3(10, pos.y, 0);
+    }
+}
---
+++ Assets/Script/SceneMove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    //Sceneの移動
+    public void Move(string Move_left, string Move_right, string Go_Scene) {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            //右のページに移動
+            SceneManager.LoadScene(Move_left);
+        } else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            //左のページに移動
+            SceneManager.LoadScene(Move_right);
+        } else if (Input.GetKeyDown(KeyCode.Space)) {
+            //そのステージをプレイ
+            SceneManager.LoadScene(Go_Scene);
+        }
+    }
+    // Update is called once per frame
+    void Update() {
+        //ホーム移動
+        if(SceneManager.GetActiveScene().name == "Home") {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                SceneManager.LoadScene("Scene_egg");
+            }        
+        }
+        //ホームに戻る
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            SceneManager.LoadScene("Home");
+        }
+
+        //ゲームキャラクター選択
+        if (SceneManager.GetActiveScene().name == "Scene_apple") {
+            //Scene_appleの場合の移動場所の設定
+            Move("Scene_egg", "Scene_ghost", "Play_apple");
+        }
+        else if (SceneManager.GetActiveScene().name == "Scene_ghost") {
+            //Scene_ghostの場合の移動場所の設定
+            Move("Scene_apple", "Scene_grape", "Play_ghost");
+        }
+        else if (SceneManager.GetActiveScene().name == "Scene_grape") {
+            //Scene_grapeの場合の移動場所の設定
+            Move("Scene_ghost", "Scene_egg", "Play_grape");
+        } 
+        else if (SceneManager.GetActiveScene().name == "Scene_egg") {
+            //Scene_eggの場合の移動場所の設定
+            Move("Scene_grape", "Scene_apple", "Play_egg");
+        }
+    }
+}
+
