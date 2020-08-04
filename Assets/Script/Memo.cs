de3619af4fffce6fc72900c99664d41517dc2f32
hatomugi1127 <hatomugi.work@gmail.com>
2020-08-05 01:32:11 +0900

    Change_memo
    メモの内容を変更
    操作キャラクターの移動方法についてqiitaから持ってきたif文の少ないものを用意したので、確認後、自分のスクリプトに合わせてください。

--- Assets/Script/Memo.cs
+++ Assets/Script/Memo.cs
@@ -7,3 +7,16 @@
 //eggがeasy
 // 20/03/02に弾幕、キャラクターの移動のプログラム完成
 // 20/07/15に調整、かつ、説明の追加
+// 20/08/05 UnityからGitHubにコミット、プッシュの練習
+// 操作キャラの移動方法に関するscriptがifを使いすぎなので、できるだけシンプルにしたい
+// qiitaより、
+// void Update() {
+    // var a = Input.GetAxis("Horizontal");//左右
+    // var b = Input.GetAxis("Vertical");//上下
+    // var force = new Vector3(a, 0f, b);//(x, y, z)平面方向がx,z上空方向がy
+    // GetComponent<Rigidbody>().AddForce(force * 2000);
+    // if (Input.GetMouseButton(0) == true) {
+        // this.transform.Rotate(0, 5, 0);
+    // }
+// }
+//このような文もある
